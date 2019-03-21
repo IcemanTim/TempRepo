5ea050a0607558a63cdaf7ecc0bdb4e54207452c
BikbulatovTimur <bikbulatovtimur96@yandex.ru>
2019-03-21 15:32:33 +0300

    work with colours.txt

--- example03.py
+++ example03.py
@@ -6,12 +6,26 @@
 '''
 
 from tkinter import *
+import string
+
+def change():
+    global colour 
+    global colourselection
+    global count 
+    if (count < 2 ):
+        colour = colourselection[count]
+        button.configure(bg = colour)
+        count + 1
+    else:
+        colour = colourselection[count]
+        button.configure(bg = colour)
+        count = 1 
 
 def show_msg(event):
     print ("Second Button!")
 
 def add_two_buttons(event):
-	But = Button(root, text="Press and change my colour!")
+	But = Button(root, text="Press and change the label's colour!")
 	But.bind('<Button-1>', show_msg)
 	But.grid(row=0, column=0, sticky=E+W+S+N)
 
@@ -54,6 +68,45 @@ BG_label.grid(row=1, column=0, columnspan=3, sticky=E+W+N+S)
 
 Txt = Label(root, text="This is a label", bg="PeachPuff")
 Txt.grid(row=2, column=0, columnspan=3, sticky=E+W+N+S)
+
+#----------------------------------------------------
+
+f = open('colours.txt', 'r')
+
+global colours
+colours = []
+global count 
+count = 1
+
+colour = ""
+
+for line in f:
+	l = len(line)
+	arr = []
+	i = 0
+	while i < l:
+		mass = ""
+		a = line[i]
+		while (a != ' ' or a != '\t') and '0' <= a <= '9':
+			mass += a
+			i += 1
+			if i < l:
+				a = line[i]
+			else:
+				break
+		if a in string.ascii_letters:
+			while a != '\n' and i < l:
+				mass += a
+				i += 1
+				if i < l:
+					a = line[i]
+		i += 1
+		if mass != '':
+			if mass.isdigit() :
+				arr.append(int(mass))
+			else :
+				arr.append(mass)
+	colours.append(arr)
 
 TKroot.mainloop()
 print("Done")
